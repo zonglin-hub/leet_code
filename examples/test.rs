df3d9f883c643d67dac438fcb3e487a671751788
liuzonglin <lzlysfxx@163.com>
2023-08-08 12:58:16 +0800

    up

--- examples/test.rs
+++ examples/test.rs
@@ -1,73 +1,12 @@
-// Definition for singly-linked list.
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Box<ListNode>>,
+fn main() {
+    let mut x = 1;
+    x <<= 1; // x *= 2;
+    x = (x << 1) + (x << 3); // x *= 10;
+    x >>= 1; // x /= 2;
+    println!("{}", x);
+
+    let mut a = 5;
+    let mut b = 3;
+    a ^= b; b ^= a; a ^= b; // 交换两个值
+    println!("{} {}", a, b);
 }
-
-impl ListNode {
-    #[inline]
-    fn new(val: i32) -> Self {
-        ListNode { next: None, val }
-    }
-}
-
-pub struct Solution;
-
-impl Solution {
-    pub fn reverse_k_group(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
-        fn reverse(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-            let mut pre = None;
-            let mut head = head;
-            let mut next;
-            while head.is_some() {
-                next = head.as_mut().unwrap().next.take();
-                head.as_mut().unwrap().next = pre.take();
-                pre = head.take();
-                head = next.take()
-            }
-            pre
-        }
-
-        if k == 1 {
-            return head;
-        }
-
-        let mut dummy = Some(Box::new(ListNode::new(0)));
-        dummy.as_mut().unwrap().next = head;
-        let mut pre: *mut _ = &mut dummy;
-        let mut end: *mut _ = &mut dummy;
-
-        unsafe {
-            while end.as_ref().unwrap().as_ref().unwrap().next.is_some() {
-                for _ in 0..k {
-                    if end.as_ref().unwrap().is_none() {
-                        break;
-                    }
-                    end = &mut end.as_mut().unwrap().as_mut().unwrap().next;
-                }
-                if end.as_ref().unwrap().is_none() {
-                    break;
-                }
-
-                let mut start = pre.as_mut().unwrap().as_mut().unwrap().next.take();
-                pre = &mut dummy;
-
-                while pre.as_ref().unwrap().as_ref().unwrap().next.is_some() {
-                    pre = &mut pre.as_mut().unwrap().as_mut().unwrap().next;
-                }
-
-                let mut next = end.as_mut().unwrap().as_mut().unwrap().next.take();
-                let mut startp: *mut _ = &mut start;
-                end.as_mut().unwrap().as_mut().unwrap().next = None;
-                pre.as_mut().unwrap().as_mut().unwrap().next = reverse(start).take();
-                startp.as_mut().unwrap().as_mut().unwrap().next = next.take();
-                pre = startp;
-                end = pre;
-            }
-        }
-        dummy.as_mut().unwrap().next.take()
-    }
-}
-
-fn main() {}
