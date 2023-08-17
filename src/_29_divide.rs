e98af45f2207baf724e7ce0a6b8247d36e4cabc6
liuzonglin <lzlysfxx@163.com>
2023-08-17 19:18:00 +0800

    新增 29

---
+++ src/_29_divide.rs
@@ -0,0 +1,44 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn divide(dividend: i32, divisor: i32) -> i32 {
+        let is_neg = (dividend > 0) != (divisor > 0);
+        let mut dividend = (dividend as i64).abs();
+        let mut divisor = (divisor as i64).abs();
+        let mut ans = 0;
+        let mut cnt = 1;
+
+        while divisor <= dividend {
+            cnt <<= 1;
+            divisor <<= 1;
+        }
+
+        while cnt > 0 {
+            if dividend >= divisor {
+                ans += cnt;
+                dividend -= divisor;
+            }
+
+            divisor >>= 1;
+            cnt >>= 1;
+        }
+
+        if is_neg {
+            -ans as i32
+        } else {
+            ans.min(i32::MAX as i64) as i32
+        }
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_divide() {
+        assert_eq!(Solution::divide(10, 3), 3);
+        assert_eq!(Solution::divide(7, -3), -2);
+    }
+}
