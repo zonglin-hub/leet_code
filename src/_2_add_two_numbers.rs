9b0ae17e9beea4b4f8c4be473d8c7f01f5a34063
liuzonglin <lzlysfxx@163.com>
2023-09-06 10:28:16 +0800

    添加注释

--- src/_2_add_two_numbers.rs
+++ src/_2_add_two_numbers.rs
@@ -22,13 +22,7 @@ impl ListNode {
 }
 
 impl Solution {
-    /// 将两个链表l1和l2合并成一个链表，同时处理进位问题。
-    ///
-    /// 首先，我们检查l1和l2是否都为None，并且carry是否为0。
-    /// 如果是，那么如果链表为空，返回None；
-    /// 否则，创建一个新的节点，其值为carry % 10，
-    /// 并将carry / 10赋值给next指针。
-    ///
+    /// 递归
     pub fn add_two_numbers(
         l1: Option<Box<ListNode>>,
         l2: Option<Box<ListNode>>,
@@ -41,9 +35,24 @@ impl Solution {
         l2: Option<Box<ListNode>>,
         mut carry: i32,
     ) -> Option<Box<ListNode>> {
+        // if l1.is_none() && l2.is_none() && carry == 0 {
+        //     None
+        // } else {
+        //     Some(Box::new(ListNode {
+        //         next: carried(
+        //             l1.and_then(|x| {carry += x.val; x.next}),
+        //             l2.and_then(|x| {carry += x.val; x.next}),
+        //             carry / 10
+        //         ),
+        //         val: carry % 10
+        //     }))
+        // }
+        // 首先，我们检查l1和l2是否都为None，并且carry是否为0。
         match l1.is_none() && l2.is_none() && carry == 0 {
+            // 如果是，那么如果链表为空，返回None；
             true => None,
             false => Some(Box::new(ListNode {
+                // 如果carry为0，则将l1和l2的值相加，并将结果放入到l1的next中，将carry减1
                 next: Self::carried(
                     l1.and_then(|x| {
                         carry += x.val;
@@ -53,78 +62,79 @@ impl Solution {
                         carry += x.val;
                         x.next
                     }),
+                    // 并将carry / 10赋值给next指针。
                     carry / 10,
                 ),
+                // 否则，创建一个新的节点，其值为carry % 10，
                 val: carry % 10,
             })),
         }
     }
 }
 
-pub fn create_list(nums: Vec<i32>) -> Option<Box<ListNode>> {
-    let mut head = Some(Box::new(ListNode::new(nums[0])));
-    let mut p = head.as_mut();
-    for num in nums.iter().skip(1) {
-        let node = Some(Box::new(ListNode::new(*num)));
-        p.as_mut().unwrap().next = node;
-        p = p.unwrap().next.as_mut();
-    }
-    head
-}
-
-pub fn to_vec(head: Option<Box<ListNode>>) -> Vec<i32> {
-    let mut res = vec![];
-    let mut p = head;
-    while let Some(node) = p {
-        res.push(node.val);
-        p = node.next;
-    }
-    res
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    /// 两个链表都为空
+    pub fn create_list(nums: Vec<i32>) -> Option<Box<ListNode>> {
+        // 创建一个头节点，并将其赋值给head
+        let mut head = Some(Box::new(ListNode::new(nums[0])));
+        // 将head赋值给p
+        let mut p = head.as_mut();
+        // 遍历nums数组，将每一个元素赋值给ListNode
+        for num in nums.iter().skip(1) {
+            let node = Some(Box::new(ListNode::new(*num)));
+            // 将ListNode赋值给p的下一个节点
+            p.as_mut().unwrap().next = node;
+            // 将p的下一个节点赋值给p
+            p = p.unwrap().next.as_mut();
+        }
+        // 返回head
+        head
+    }
+
+    pub fn to_vec(head: Option<Box<ListNode>>) -> Vec<i32> {
+        // 创建一个空的数组，用于存放节点的值
+        let mut res = vec![];
+        // 将head赋值给p
+        let mut p = head;
+        // 当p存在时，将其值存入res数组
+        while let Some(node) = p {
+            res.push(node.val);
+            // 将p的下一个节点赋值给p
+            p = node.next;
+        }
+        // 返回res数组
+        res
+    }
+
     #[test]
-    fn test_empty() {
+    fn test_add_two_numbers() {
+        // 两个链表都为空
         let l1 = None;
         let l2 = None;
         let expected = None;
         assert_eq!(Solution::add_two_numbers(l1, l2), expected);
-    }
 
-    /// 其中一个链表为空，另一个不为空
-    #[test]
-    fn test_one_empty() {
+        // 其中一个链表为空，另一个不为空
         let l1 = create_list(vec![2, 4, 3]);
         let l2 = None;
         let expected = create_list(vec![2, 4, 3]);
         assert_eq!(Solution::add_two_numbers(l1, l2), expected);
-    }
 
-    /// 两个链表长度相等
-    #[test]
-    fn test_same_length() {
+        // 两个链表长度相等
         let l1 = create_list(vec![2, 4, 3]);
         let l2 = create_list(vec![5, 6, 4]);
         let expected = create_list(vec![7, 0, 8]);
         assert_eq!(to_vec(Solution::add_two_numbers(l1, l2)), to_vec(expected));
-    }
 
-    /// 两个链表长度不相等
-    #[test]
-    fn test_different_length() {
+        // 两个链表长度不相等
         let l1 = create_list(vec![9, 9, 9, 9, 9]);
         let l2 = create_list(vec![1]);
         let expected = create_list(vec![0, 0, 0, 0, 0, 1]);
         assert_eq!(to_vec(Solution::add_two_numbers(l1, l2)), to_vec(expected));
-    }
 
-    /// 两个链表相加进位
-    #[test]
-    fn test_carry() {
+        // 两个链表相加进位
         let l1 = create_list(vec![9, 9, 9, 9, 9]);
         let l2 = create_list(vec![9, 9, 9, 9, 9]);
         let expected = create_list(vec![8, 9, 9, 9, 9, 1]);
