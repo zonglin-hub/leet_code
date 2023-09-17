830e10158470c6d2b31c9113a4ce38cbee207f62
liuzonglin <lzlysfxx@163.com>
2023-09-17 22:56:51 +0800

    相同题型归纳，增加 136 测试用例

--- tests/leet_code/test_136_single_number.rs
+++ tests/leet_code/test_136_single_number.rs
@@ -5,4 +5,6 @@ fn test_single_number() {
     assert_eq!(Solution::single_number(vec![2, 2, 1]), 1);
     assert_eq!(Solution::single_number(vec![4, 1, 2, 1, 2]), 4);
     assert_eq!(Solution::single_number(vec![1]), 1);
+    assert_eq!(Solution::single_number(vec![1, 1, 2, 3, 3, 4, 4, 8, 8]), 2);
+    assert_eq!(Solution::single_number(vec![3, 3, 7, 7, 10, 11, 11]), 10);
 }
