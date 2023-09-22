ffef4c715183918382806e044b1e3e9631907a67
liuzonglin <lzlysfxx@163.com>
2023-09-22 17:06:28 +0800

    优化代码，满足 输入 23， 2 输出 1

--- src/leet_code/_2591_dist_money.rs
+++ src/leet_code/_2591_dist_money.rs
@@ -1,21 +1,20 @@
 use super::Solution;
 
 impl Solution {
-    pub fn dist_money(money: i32, children: i32) -> i32 {
+    pub fn dist_money(mut money: i32, mut children: i32) -> i32 {
         if money < children {
             return -1;
         }
 
-        let count = (money - children) / 7;
-        let money_left = money - count * 7 - children;
-        let children_left = children - count;
-        let result =
-            if children_left == 0 && money_left > 0 || children_left == 1 && money_left == 3 {
-                count - 1
-            } else {
-                count
-            };
+        money -= children;
+        let count = children.min(money / 7);
+        money -= count * 7;
+        children -= count;
 
-        result.max(0)
+        if children == 0 && money > 0 || children == 1 && money == 3 {
+            count - 1
+        } else {
+            count
+        }
     }
 }
--- tests/leet_code/test_2591_dist_money.rs
+++ tests/leet_code/test_2591_dist_money.rs
@@ -4,4 +4,5 @@ use leet_code::leet_code::Solution;
 fn test_dist_money() {
     assert_eq!(Solution::dist_money(20, 3), 1);
     assert_eq!(Solution::dist_money(16, 2), 2);
+    assert_eq!(Solution::dist_money(23, 2), 1);
 }
