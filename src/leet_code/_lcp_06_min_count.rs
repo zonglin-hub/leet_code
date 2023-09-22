ac6ccee8cdc8c7b756cdba99649c9df17681752f
liuzonglin <lzlysfxx@163.com>
2023-09-22 15:23:32 +0800

    代码转换成 函数式

--- src/leet_code/_lcp_06_min_count.rs
+++ src/leet_code/_lcp_06_min_count.rs
@@ -2,10 +2,6 @@ use super::Solution;
 
 impl Solution {
     pub fn min_count(coins: Vec<i32>) -> i32 {
-        let mut sum = 0;
-        for i in coins {
-            sum += (i + 1) / 2
-        }
-        sum
+        coins.iter().fold(0, |sum, &x| (x + 1) / 2 + sum)
     }
 }
