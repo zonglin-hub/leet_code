c4900b145b7f882b30f2148e246f09392f27f574
liuzonglin <lzlysfxx@163.com>
2023-10-06 11:58:29 +0800

    移除了 IT: Iterator 的类型注解， 并直接指定了 Item 的类型为 &'a i32。 将 loop 循环替换为了 while 循环

--- src/leet_code/_105_build_tree.rs
+++ src/leet_code/_105_build_tree.rs
@@ -8,28 +8,26 @@ impl Solution {
     pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> TreeNodePtr {
         fn build<'a, IT>(inorder: &[i32], mut preorder: IT) -> (TreeNodePtr, IT)
         where
-            IT: Iterator,
-            <IT as Iterator>::Item: Into<&'a i32>,
+            IT: Iterator<Item = &'a i32>,
         {
             let len = inorder.len();
             if len == 0 {
                 (None, preorder)
             } else {
-                let root = preorder.next().expect("").into();
+                // 编译器无法推断出 root 的具体类型。这可能是因为 preorder.next() 返回的是一个泛型迭代器项，而 into() 方法需要具体的类型信息
+                let root: Option<&i32> = preorder.next();
                 let mut pos = 0;
 
-                loop {
-                    if inorder[pos] == *root {
-                        break;
-                    }
+                while pos < inorder.len() && inorder[pos] != *root.expect("") {
                     pos += 1;
                 }
+
                 let (left, preorder) = build(&inorder[0..pos], preorder);
                 let (right, preorder) = build(&inorder[pos + 1..len], preorder);
 
                 (
                     Some(Rc::new(RefCell::new(TreeNode {
-                        val: *root,
+                        val: *root.expect(""),
                         left,
                         right,
                     }))),
