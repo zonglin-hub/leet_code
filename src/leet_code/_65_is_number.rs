586a86e3f379f5a454d7992026e57965bc1242bf
liuzonglin <lzlysfxx@163.com>
2023-11-04 10:48:10 +0800

    add 65

---
+++ src/leet_code/_65_is_number.rs
@@ -0,0 +1,127 @@
+use super::Solution;
+
+enum State {
+    Start,
+    Sign,
+    Integer,
+    Dot,
+    InitialDot,
+    Decimal,
+    E,
+    ESign,
+    EInteger,
+    Illegal,
+}
+
+impl State {
+    fn next(&self, next: Input) -> Self {
+        match self {
+            State::Start => match next {
+                Input::Digit => State::Integer,
+                Input::Dot => State::InitialDot,
+                Input::Sign => State::Sign,
+                _ => State::Illegal,
+            },
+            State::Sign => match next {
+                Input::Digit => State::Integer,
+                Input::Dot => State::InitialDot,
+                _ => State::Illegal,
+            },
+            State::Integer => match next {
+                Input::Digit => State::Integer,
+                Input::Dot => State::Dot,
+                Input::E => State::E,
+                _ => State::Illegal,
+            },
+            State::Dot => match next {
+                Input::Digit => State::Decimal,
+                Input::E => State::E,
+                _ => State::Illegal,
+            },
+            State::InitialDot => match next {
+                Input::Digit => State::Decimal,
+                _ => State::Illegal,
+            },
+            State::Decimal => match next {
+                Input::Digit => State::Decimal,
+                Input::E => State::E,
+                _ => State::Illegal,
+            },
+            State::E => match next {
+                Input::Digit => State::EInteger,
+                Input::Sign => State::ESign,
+                _ => State::Illegal,
+            },
+            State::ESign => match next {
+                Input::Digit => State::EInteger,
+                _ => State::Illegal,
+            },
+            State::EInteger => match next {
+                Input::Digit => State::EInteger,
+                _ => State::Illegal,
+            },
+            _ => State::Illegal,
+        }
+    }
+
+    fn accept(&self) -> bool {
+        // match self {
+        //     State::Integer | State::Dot | State::Decimal | State::EInteger => true,
+        //     _ => false,
+        // }
+        matches!(
+            self,
+            State::Integer | State::Dot | State::Decimal | State::EInteger
+        )
+    }
+}
+
+enum Input {
+    Digit,
+    Sign,
+    Dot,
+    E,
+    Other,
+}
+
+impl<T> From<T> for Input
+where
+    T: Into<char>,
+{
+    fn from(value: T) -> Self {
+        match value.into() {
+            '0'..='9' => Input::Digit,
+            '.' => Input::Dot,
+            '+' | '-' => Input::Sign,
+            'e' | 'E' => Input::E,
+            _ => Input::Other,
+        }
+    }
+}
+
+impl Solution {
+    pub fn _is_number(s: String) -> bool {
+        s.trim().parse::<f32>().is_ok()
+    }
+
+    pub fn is_number(s: String) -> bool {
+        let mut state = State::Start;
+        s.chars().for_each(|f| state = state.next(f.into()));
+        state.accept()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::leet_code::Solution;
+
+    #[test]
+    fn test_is_number() {
+        assert!(Solution::is_number("0".to_string()));
+        assert!(Solution::is_number("-123.456e789".to_string()));
+        assert_eq!(Solution::is_number("e".to_string()), false);
+        assert_eq!(Solution::is_number(".".to_string()), false);
+        assert_eq!(Solution::is_number("95a54e53".to_string()), false);
+        assert_eq!(Solution::is_number("99e2.5".to_string()), false);
+    }
+}
--- src/leet_code/mod.rs
+++ src/leet_code/mod.rs
@@ -110,6 +110,7 @@ pub mod _61_rotate_right;
 pub mod _62_unique_paths;
 pub mod _63_unique_paths_with_obstacles;
 pub mod _64_min_path_sum;
+pub mod _65_is_number;
 pub mod _680_valid_palindrome;
 pub mod _6_convert;
 pub mod _70_climb_stairs;
