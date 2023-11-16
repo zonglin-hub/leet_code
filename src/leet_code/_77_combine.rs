4050f5a55230e9fa850e8d3789ca090bc939e34c
liuzonglin <lzlysfxx@163.com>
2023-11-16 11:43:18 +0800

    add 77

---
+++ src/leet_code/_77_combine.rs
@@ -0,0 +1,50 @@
+use super::Solution;
+
+impl Solution {
+    pub fn combine(n: i32, k: i32) -> Vec<Vec<i32>> {
+        fn dfs(cur: i32, n: i32, k: i32, tmp: &mut Vec<i32>, ans: &mut Vec<Vec<i32>>) {
+            if tmp.len() + ((n - cur + 1) as usize) < k as usize {
+                return;
+            }
+
+            if tmp.len() == k as usize {
+                return ans.push(tmp.to_vec());
+            }
+
+            tmp.push(cur);
+
+            dfs(cur + 1, n, k, tmp, ans);
+
+            tmp.pop();
+
+            dfs(cur + 1, n, k, tmp, ans);
+        }
+
+        let mut tmp = Vec::new();
+        let mut ans = Vec::new();
+        dfs(1, n, k, &mut tmp, &mut ans);
+        ans
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::leet_code::Solution;
+
+    #[test]
+    fn test_combine() {
+        let mut out = vec![
+            vec![2, 4],
+            vec![3, 4],
+            vec![2, 3],
+            vec![1, 2],
+            vec![1, 3],
+            vec![1, 4],
+        ];
+        out.sort();
+        assert_eq!(Solution::combine(4, 2), out);
+
+        let out = vec![vec![1]];
+        assert_eq!(Solution::combine(1, 1), out);
+    }
+}
