53e6e7ce679e15b09487f11e14ed5805784e990b
liuzonglin <lzlysfxx@163.com>
2024-01-14 14:19:13 +0800

    移除内存异常算法

--- src/leet_code/_127_ladder_length.rs
+++ src/leet_code/_127_ladder_length.rs
@@ -3,37 +3,56 @@ use super::Solution;
 use std::collections::{HashSet, VecDeque};
 
 impl Solution {
-    #[allow(clippy::needless_range_loop)]
     pub fn ladder_length(begin_word: String, end_word: String, word_list: Vec<String>) -> i32 {
-        let mut word_set = word_list.into_iter().collect::<HashSet<String>>();
-        if !word_set.contains(&end_word) {
-            return 0;
-        }
+        #[inline]
+        fn check(word1: &[u8], word2: &[u8]) -> bool {
+            let len = word1.len();
+            let mut diff_count = 0;
 
-        let mut word_queue = VecDeque::new();
-        word_queue.push_back((begin_word, 1));
-
-        while let Some((mut src, idx)) = word_queue.pop_front() {
-            if end_word.eq(&src) {
-                return idx;
+            for i in 0..len {
+                if word1[i] != word2[i] {
+                    diff_count += 1;
+                    if diff_count > 1 {
+                        return false;
+                    }
+                }
             }
 
-            let u8arr = unsafe { &mut *(src.as_bytes_mut() as *mut [u8]) };
+            diff_count == 1
+        }
 
-            for i in 0..u8arr.len() {
-                let origin_c = u8arr[i];
+        let mut word_list = word_list.into_iter().collect::<HashSet<_>>();
+        if !word_list.contains(&end_word) {
+            return 0;
+        }
+        word_list.remove(&begin_word);
 
-                for c in b'a'..=b'z' {
-                    if origin_c != c {
-                        u8arr[i] = c;
-                        if let Some(new_src) = word_set.take(&src) {
-                            word_queue.push_back((new_src, idx + 1));
+        let mut min_count = 2;
+        let mut queue = VecDeque::new();
+        queue.push_back(begin_word);
+
+        while !queue.is_empty() {
+            let size = queue.len();
+
+            for _ in 0..size {
+                let cur_word = queue.pop_front().unwrap();
+                let mut temp_word_list = HashSet::new();
+
+                for word in word_list.into_iter() {
+                    if check(cur_word.as_bytes(), word.as_bytes()) {
+                        if word == end_word {
+                            return min_count;
                         }
+                        queue.push_back(word);
+                    } else {
+                        temp_word_list.insert(word);
                     }
                 }
 
-                u8arr[i] = origin_c;
+                word_list = temp_word_list;
             }
+
+            min_count += 1;
         }
 
         0
--- src/leet_code/_707_my_linked_list.rs
+++ src/leet_code/_707_my_linked_list.rs
@@ -1,146 +1,146 @@
-#![allow(unused)]
+// #![allow(unused)]
 
-use std::{
-    cell::{Ref, RefCell},
-    future::IntoFuture,
-    io::SeekFrom,
-    rc::Rc,
-};
+// use std::{
+//     cell::{Ref, RefCell},
+//     future::IntoFuture,
+//     io::SeekFrom,
+//     rc::Rc,
+// };
 
-type ListNodePtr = Option<Rc<RefCell<ListNode>>>;
+// type ListNodePtr = Option<Rc<RefCell<ListNode>>>;
 
-struct ListNode {
-    val: i32,
-    prev: ListNodePtr,
-    next: ListNodePtr,
-}
+// struct ListNode {
+//     val: i32,
+//     prev: ListNodePtr,
+//     next: ListNodePtr,
+// }
 
-impl ListNode {
-    fn new(val: i32) -> Self {
-        Self { val, prev: None, next: None }
-    }
-}
+// impl ListNode {
+//     fn new(val: i32) -> Self {
+//         Self { val, prev: None, next: None }
+//     }
+// }
 
-struct MyLinkedList {
-    head: ListNodePtr,
-    size: i32,
-}
+// struct MyLinkedList {
+//     head: ListNodePtr,
+//     size: i32,
+// }
 
-impl MyLinkedList {
-    fn new() -> Self {
-        let mut head = Rc::new(RefCell::new(ListNode::new(1)));
-        head.borrow_mut().prev = Some(head.clone());
-        head.borrow_mut().next = Some(head.clone());
+// impl MyLinkedList {
+//     fn new() -> Self {
+//         let mut head = Rc::new(RefCell::new(ListNode::new(1)));
+//         head.borrow_mut().prev = Some(head.clone());
+//         head.borrow_mut().next = Some(head.clone());
 
-        Self { head: Some(head), size: 0 }
-    }
+//         Self { head: Some(head), size: 0 }
+//     }
 
-    fn find_node(&self, index: i32) -> ListNodePtr {
-        if self.size == 0 {
-            return self.head.as_ref().unwrap().borrow().next.clone();
-        }
+//     fn find_node(&self, index: i32) -> ListNodePtr {
+//         if self.size == 0 {
+//             return self.head.as_ref().unwrap().borrow().next.clone();
+//         }
 
-        if self.size == index {
-            return self.head.as_ref().unwrap().borrow().prev.clone();
-        }
+//         if self.size == index {
+//             return self.head.as_ref().unwrap().borrow().prev.clone();
+//         }
 
-        let mut curr = self.head.as_ref().unwrap().borrow().next.clone();
-        for _ in 0..index {
-            let node = curr.as_ref().unwrap().borrow().next.clone();
-            curr = node;
-        }
+//         let mut curr = self.head.as_ref().unwrap().borrow().next.clone();
+//         for _ in 0..index {
+//             let node = curr.as_ref().unwrap().borrow().next.clone();
+//             curr = node;
+//         }
 
-        curr
-    }
+//         curr
+//     }
 
-    fn get(&self, index: i32) -> i32 {
-        if index < 0 || index >= self.size {
-            return -1;
-        }
+//     fn get(&self, index: i32) -> i32 {
+//         if index < 0 || index >= self.size {
+//             return -1;
+//         }
 
-        self.find_node(index).as_ref().unwrap().borrow().val
-    }
+//         self.find_node(index).as_ref().unwrap().borrow().val
+//     }
 
-    fn add_at_head(&mut self, val: i32) {
-        let next = self.head.as_ref().unwrap().borrow().next.clone();
-        let mut curr =
-            Rc::new(RefCell::new(ListNode { val, prev: self.head.clone(), next: next.clone() }));
-        next.clone().as_ref().unwrap().borrow_mut().prev = Some(curr.clone());
-        self.head.as_ref().unwrap().borrow_mut().next = Some(curr.clone());
-        self.size += 1;
-    }
+//     fn add_at_head(&mut self, val: i32) {
+//         let next = self.head.as_ref().unwrap().borrow().next.clone();
+//         let mut curr =
+//             Rc::new(RefCell::new(ListNode { val, prev: self.head.clone(), next: next.clone() }));
+//         next.clone().as_ref().unwrap().borrow_mut().prev = Some(curr.clone());
+//         self.head.as_ref().unwrap().borrow_mut().next = Some(curr.clone());
+//         self.size += 1;
+//     }
 
-    fn add_at_tail(&mut self, val: i32) {
-        let mut prev = self.head.as_ref().unwrap().borrow().prev.clone();
-        let mut curr =
-            Rc::new(RefCell::new(ListNode { val, prev: prev.clone(), next: self.head.clone() }));
-        prev.clone().as_ref().unwrap().borrow_mut().next = Some(curr.clone());
-        self.head.as_ref().unwrap().borrow_mut().prev = Some(curr.clone());
-        self.size += 1;
-    }
+//     fn add_at_tail(&mut self, val: i32) {
+//         let mut prev = self.head.as_ref().unwrap().borrow().prev.clone();
+//         let mut curr =
+//             Rc::new(RefCell::new(ListNode { val, prev: prev.clone(), next: self.head.clone() }));
+//         prev.clone().as_ref().unwrap().borrow_mut().next = Some(curr.clone());
+//         self.head.as_ref().unwrap().borrow_mut().prev = Some(curr.clone());
+//         self.size += 1;
+//     }
 
-    fn add_at_index(&mut self, index: i32, val: i32) {
-        if index > self.size {
-            return;
-        }
+//     fn add_at_index(&mut self, index: i32, val: i32) {
+//         if index > self.size {
+//             return;
+//         }
 
-        if index < 0 {
-            return self.add_at_head(val);
-        }
+//         if index < 0 {
+//             return self.add_at_head(val);
+//         }
 
-        if index == self.size {
-            return self.add_at_tail(val);
-        }
+//         if index == self.size {
+//             return self.add_at_tail(val);
+//         }
 
-        let curr = self.find_node(index);
-        let node = Rc::new(RefCell::new(ListNode {
-            val,
-            prev: curr.as_ref().unwrap().borrow().prev.clone(),
-            next: curr.clone(),
-        }));
+//         let curr = self.find_node(index);
+//         let node = Rc::new(RefCell::new(ListNode {
+//             val,
+//             prev: curr.as_ref().unwrap().borrow().prev.clone(),
+//             next: curr.clone(),
+//         }));
 
-        curr.as_ref().unwrap().borrow_mut().prev.as_ref().unwrap().borrow_mut().next =
-            Some(node.clone());
+//         curr.as_ref().unwrap().borrow_mut().prev.as_ref().unwrap().borrow_mut().next =
+//             Some(node.clone());
 
-        curr.as_ref().unwrap().borrow_mut().prev = Some(node.clone());
-        self.size += 1;
-    }
+//         curr.as_ref().unwrap().borrow_mut().prev = Some(node.clone());
+//         self.size += 1;
+//     }
 
-    fn delete_at_index(&mut self, index: i32) {
-        if self.size <= 0 || index < 0 || index >= self.size {
-            return;
-        }
+//     fn delete_at_index(&mut self, index: i32) {
+//         if self.size <= 0 || index < 0 || index >= self.size {
+//             return;
+//         }
 
-        if self.size == 1 {
-            self.head.as_ref().unwrap().borrow_mut().prev = self.head.clone();
-            self.head.as_ref().unwrap().borrow_mut().next = self.head.clone();
-            self.size -= 1;
-        }
+//         if self.size == 1 {
+//             self.head.as_ref().unwrap().borrow_mut().prev = self.head.clone();
+//             self.head.as_ref().unwrap().borrow_mut().next = self.head.clone();
+//             self.size -= 1;
+//         }
 
-        let curr = self.find_node(index);
-        let (prev, next) = (
-            curr.as_ref().unwrap().borrow().prev.clone(),
-            curr.as_ref().unwrap().borrow().next.clone(),
-        );
-        prev.clone().as_ref().unwrap().borrow_mut().next = next.clone();
-        next.clone().as_ref().unwrap().borrow_mut().prev = prev.clone();
-        self.size -= 1;
-    }
-}
+//         let curr = self.find_node(index);
+//         let (prev, next) = (
+//             curr.as_ref().unwrap().borrow().prev.clone(),
+//             curr.as_ref().unwrap().borrow().next.clone(),
+//         );
+//         prev.clone().as_ref().unwrap().borrow_mut().next = next.clone();
+//         next.clone().as_ref().unwrap().borrow_mut().prev = prev.clone();
+//         self.size -= 1;
+//     }
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::MyLinkedList;
+// #[cfg(test)]
+// mod tests {
+//     use super::MyLinkedList;
 
-    #[test]
-    fn test_my_linked_list() {
-        let mut obj = MyLinkedList::new();
-        obj.add_at_head(1);
-        obj.add_at_tail(3);
-        obj.add_at_index(1, 2); // 链表变为 1->2->3
-        assert_eq!(obj.get(1), 2); // 返回 2
+//     #[test]
+//     fn test_my_linked_list() {
+//         let mut obj = MyLinkedList::new();
+//         obj.add_at_head(1);
+//         obj.add_at_tail(3);
+//         obj.add_at_index(1, 2); // 链表变为 1->2->3
+//         assert_eq!(obj.get(1), 2); // 返回 2
 
-        obj.delete_at_index(1); // 现在，链表变为 1->3
-        assert_eq!(obj.get(1), 3); // 返回 3
-    }
-}
+//         obj.delete_at_index(1); // 现在，链表变为 1->3
+//         assert_eq!(obj.get(1), 3); // 返回 3
+//     }
+// }
