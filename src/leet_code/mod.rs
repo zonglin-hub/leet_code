c9d25b17d0ec9c1f812a59f216635803b77898d2
liuzonglin <lzlysfxx@163.com>
2024-04-17 00:02:20 +0800

    新增 leet code 924

--- src/leet_code/mod.rs
+++ src/leet_code/mod.rs
@@ -195,6 +195,7 @@ pub mod _0746_min_cost_climbing_stairs;
 pub mod _0856_score_of_parentheses;
 pub mod _0890_find_and_replace_pattern;
 pub mod _0896_is_monotonic;
+pub mod _0924_min_malware_spread;
 pub mod _0961_repeated_n_times;
 pub mod _0965_is_unival_tree;
 pub mod _0990_equations_possible;
